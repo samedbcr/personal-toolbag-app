50256d8d02a7e29f44061a0236d17294fc49b5df
Samed Biçer <samed.bicer@ecodation.com>
2021-01-08 00:33:25 +0300

    anasayfa tasarimi yapildi

--- lib/theme/custom_light_theme.dart
+++ lib/theme/custom_light_theme.dart
@@ -2,19 +2,59 @@ import 'package:PersonalToolbag/theme/constants.dart';
 import 'package:flutter/material.dart';
 
 class CustomLightTheme {
-  static final ThemeData theme = ThemeData.light().copyWith(
+  static final ThemeData theme = ThemeData(
     scaffoldBackgroundColor: Colors.white,
     appBarTheme: _appBarTheme(),
+    fontFamily: 'Poppins',
+    textTheme: _textTheme(),
   );
 
-  static AppBarTheme _appBarTheme() {
-    return AppBarTheme(
-      brightness: Brightness.light,
-      elevation: 0,
-      color: Colors.white,
-      iconTheme: IconThemeData(
+  static TextTheme _textTheme() {
+    return TextTheme(
+      bodyText1: TextStyle(
+        fontSize: 15,
+        color: AppConstants.darkGrey,
+        fontWeight: FontWeight.normal,
+      ),
+      bodyText2: TextStyle(
+        fontSize: 13,
+        color: AppConstants.darkGrey,
+        fontWeight: FontWeight.normal,
+      ),
+      caption: TextStyle(
+        fontSize: 12,
+        color: AppConstants.darkGrey,
+        fontWeight: FontWeight.normal,
+      ),
+      headline4: TextStyle(
+        fontSize: 30,
         color: AppConstants.dark,
+        fontWeight: FontWeight.w500,
+      ),
+      headline5: TextStyle(
+        fontSize: 23,
+        color: AppConstants.dark,
+        fontWeight: FontWeight.normal,
+      ),
+      headline6: TextStyle(
+        fontSize: 19,
+        color: AppConstants.dark,
+        fontWeight: FontWeight.w500,
+      ),
+      button: TextStyle(
+        fontSize: 18,
+        color: AppConstants.dark,
+        fontWeight: FontWeight.w500,
       ),
     );
   }
+
+  static AppBarTheme _appBarTheme() => AppBarTheme(
+        brightness: Brightness.light,
+        elevation: 0,
+        color: Colors.white,
+        iconTheme: IconThemeData(
+          color: AppConstants.dark,
+        ),
+      );
 }
--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:PersonalToolbag/theme/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -8,7 +9,124 @@ class HomeView extends StatelessWidget {
         leading: Icon(Icons.menu),
         actions: [Icon(Icons.notifications_none)],
       ),
-      body: Container(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _buildSectionTitle(context, "Toolbag Items"),
+          buildToolbagItemsList(context),
+          _buildSectionTitle(context, "Recent Activities"),
+          _buildRecentActivitiesList(),
+        ],
+      ),
+    );
+  }
+
+  Padding _buildSectionTitle(BuildContext context, String text) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 10, 20, 20),
+      child: Text(
+        text,
+        style: Theme.of(context).textTheme.bodyText1,
+      ),
+    );
+  }
+
+  SingleChildScrollView buildToolbagItemsList(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+        child: Row(
+          children: [
+            _buildToolbagItemsCard(context, "To-Do List", "Organize your tasks",
+                "You have 2 tasks", AppConstants.gold),
+            _buildToolbagItemsCard(context, "HES Code", "Organize your codes",
+                "You have 5 codes", AppConstants.blue),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container _buildToolbagItemsCard(BuildContext context, String title,
+      String desciption, String countText, Color bgColor) {
+    return Container(
+      margin: EdgeInsets.only(right: 20),
+      width: MediaQuery.of(context).size.width * .55,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(18),
+          color: bgColor,
+          boxShadow: [
+            BoxShadow(
+              color: bgColor.withOpacity(.45),
+              blurRadius: 0,
+              spreadRadius: 0,
+              offset: Offset(6, 6),
+            ),
+          ]),
+      child: AspectRatio(
+        aspectRatio: 4 / 3,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(title, style: Theme.of(context).textTheme.headline6),
+              Text(
+                desciption,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText2
+                    .copyWith(fontWeight: FontWeight.w500),
+              ),
+              Text(
+                countText,
+                style: Theme.of(context).textTheme.caption,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Expanded _buildRecentActivitiesList() {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, i) => _buildRecentActivitiesCard(
+          i % 3 == 0
+              ? AppConstants.green
+              : i % 3 == 1
+                  ? AppConstants.purple
+                  : AppConstants.somon,
+          "New Task Added",
+          "New Task Added",
+        ),
+      ),
+    );
+  }
+
+  Container _buildRecentActivitiesCard(
+      Color bgColor, String text, String date) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      padding: EdgeInsets.symmetric(vertical: 12, horizontal: 18),
+      decoration: BoxDecoration(
+        color: bgColor.withOpacity(.30),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Row(
+        children: [
+          Icon(Icons.check),
+          SizedBox(width: 10),
+          Text(text),
+          Spacer(),
+          Text(date),
+        ],
+      ),
     );
   }
 }
