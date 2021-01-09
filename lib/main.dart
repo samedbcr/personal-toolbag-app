425e4ccbbd2ba6eb9c133dff2add4df5613f12ad
Samed Biçer <samed.bicer@ecodation.com>
2021-01-10 00:28:48 +0300

    app / android & ios icin firebaze init yapildi

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:PersonalToolbag/theme/custom_light_theme.dart';
 import 'package:PersonalToolbag/view/hes_code_view.dart';
 import 'package:PersonalToolbag/view/home_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
