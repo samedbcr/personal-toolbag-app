46f9ec4ad40a2ac7a57fe5896b9b8b6495be0308
Samed Biçer <samed.bicer@ecodation.com>
2021-01-09 21:12:56 +0300

    custom theme duzenlendi

--- lib/theme/custom_light_theme.dart
+++ lib/theme/custom_light_theme.dart
@@ -6,48 +6,46 @@ class CustomLightTheme {
     scaffoldBackgroundColor: Colors.white,
     appBarTheme: _appBarTheme(),
     fontFamily: 'Poppins',
-    textTheme: _textTheme(),
+    textTheme: _textTheme,
   );
 
-  static TextTheme _textTheme() {
-    return TextTheme(
-      bodyText1: TextStyle(
-        fontSize: 15,
-        color: AppConstants.darkGrey,
-        fontWeight: FontWeight.normal,
-      ),
-      bodyText2: TextStyle(
-        fontSize: 13,
-        color: AppConstants.darkGrey,
-        fontWeight: FontWeight.normal,
-      ),
-      caption: TextStyle(
-        fontSize: 12,
-        color: AppConstants.darkGrey,
-        fontWeight: FontWeight.normal,
-      ),
-      headline4: TextStyle(
-        fontSize: 30,
-        color: AppConstants.dark,
-        fontWeight: FontWeight.w500,
-      ),
-      headline5: TextStyle(
-        fontSize: 23,
-        color: AppConstants.dark,
-        fontWeight: FontWeight.normal,
-      ),
-      headline6: TextStyle(
-        fontSize: 19,
-        color: AppConstants.dark,
-        fontWeight: FontWeight.w500,
-      ),
-      button: TextStyle(
-        fontSize: 18,
-        color: AppConstants.dark,
-        fontWeight: FontWeight.w500,
-      ),
-    );
-  }
+  static final TextTheme _textTheme = TextTheme(
+    bodyText1: TextStyle(
+      fontSize: 15,
+      color: AppConstants.darkGrey,
+      fontWeight: FontWeight.normal,
+    ),
+    bodyText2: TextStyle(
+      fontSize: 13,
+      color: AppConstants.darkGrey,
+      fontWeight: FontWeight.normal,
+    ),
+    caption: TextStyle(
+      fontSize: 12,
+      color: AppConstants.darkGrey,
+      fontWeight: FontWeight.normal,
+    ),
+    headline4: TextStyle(
+      fontSize: 30,
+      color: AppConstants.dark,
+      fontWeight: FontWeight.w500,
+    ),
+    headline5: TextStyle(
+      fontSize: 23,
+      color: AppConstants.dark,
+      fontWeight: FontWeight.normal,
+    ),
+    headline6: TextStyle(
+      fontSize: 19,
+      color: AppConstants.dark,
+      fontWeight: FontWeight.w500,
+    ),
+    button: TextStyle(
+      fontSize: 18,
+      color: AppConstants.dark,
+      fontWeight: FontWeight.w500,
+    ),
+  );
 
   static AppBarTheme _appBarTheme() => AppBarTheme(
         brightness: Brightness.light,
